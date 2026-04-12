c2c1a174dfee393a0f2cb040f97425ea113143cf
DiegoOcsaCaceres <diego08072005@gmail.com>
2026-04-11 23:32:48 -0500

    Inicialización del Proyecto

---
+++ ProyectoAngular/ProyectoAngular/src/main/java/com/AgroLink/ProyectoAngular/ProyectoAngularApplication.java
@@ -0,0 +1,13 @@
+package com.AgroLink.ProyectoAngular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoAngularApplication.class, args);
+	}
+
+}
