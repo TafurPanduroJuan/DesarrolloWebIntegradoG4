aa4c0647e5d5171a7573276e8c0a26ebf059d355
JhesuaYola <u23209914@utp.edu.pe>
2026-04-16 22:35:48 -0500

    feat(router): configure las rutas de la aplicacion y aplique proteccion con authGuard

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,10 +3,17 @@ import { Home } from './features/home/components/home/home';
 import { Contact } from './features/contact/contact';
 import { Catalog } from './features/catalog/catalog';
 import { About } from './features/about/about';
+import { Login } from './features/auth/components/login/login';
+import { Register } from './features/auth/components/register/register';
+import { Dashboard } from './features/dashboard/dashboard';
+import { authGuard } from './shared/guards/auth.guard';
 
 export const routes: Routes = [
-  { path: '', component: Home },
-  {path: 'form', component:Contact },
-  {path: 'catalog', component: Catalog},
-  {path: 'about', component: About}
+  { path: '',          component: Home },
+  { path: 'form',      component: Contact },
+  { path: 'catalog',   component: Catalog },
+  { path: 'about',     component: About },
+  { path: 'login',     component: Login },
+  { path: 'register',  component: Register },
+  { path: 'dashboard', component: Dashboard, canActivate: [authGuard] },
 ];
