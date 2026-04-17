ab8207ca1ddc8925a59b8f90c4f80a50038725b2
JhesuaYola <u23209914@utp.edu.pe>
2026-04-16 22:04:06 -0500

    feat(auth): implemente la interfaz y logica del componente login

--- frontend/src/app/features/auth/components/login/login.ts
+++ frontend/src/app/features/auth/components/login/login.ts
@@ -1,11 +1,60 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Auth } from '../../services/auth';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, RouterLink],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
+  error = '';
+  cargando = false;
+  mostrarPassword = false;
 
+  form!: any;
+
+  constructor(
+    private fb: FormBuilder,
+    private auth: Auth,
+    private router: Router,
+  ) {
+
+    this.form = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+
+  }
+
+  get emailInvalido() {
+    return this.form.get('email')?.invalid && this.form.get('email')?.touched;
+  }
+
+  get passwordInvalido() {
+    return this.form.get('password')?.invalid && this.form.get('password')?.touched;
+  }
+
+  onSubmit() {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+    this.cargando = true;
+    this.error = '';
+
+    const { email, password } = this.form.value;
+    const resultado = this.auth.login(email!, password!);
+
+    if (resultado.ok) {
+      this.router.navigate(['/']);
+    } else {
+      this.error = resultado.mensaje;
+      this.cargando = false;
+    }
+  }
 }
