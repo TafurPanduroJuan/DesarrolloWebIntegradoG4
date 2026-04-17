5afb7b5294b22a9ca880b12d7390569a5e3dba51
JhesuaYola <u23209914@utp.edu.pe>
2026-04-16 22:33:28 -0500

    feat(shared): implemente el componente navbar con renderizado condicional de sesion

--- frontend/src/app/shared/components/navbar/navbar.ts
+++ frontend/src/app/shared/components/navbar/navbar.ts
@@ -1,13 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Auth, Usuario } from '../../../features/auth/services/auth';
 
 @Component({
   selector: 'app-navbar',
-  standalone:true,
-  imports:[CommonModule],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './navbar.html',
   styleUrls: ['./navbar.css'],
 })
 export class Navbar {
-}
+  get usuarioActual(): Usuario | null {
+    return this.auth.getUsuarioActual();
+  }
 
+  constructor(private auth: Auth) {}
+
+  cerrarSesion() {
+    this.auth.logout();
+  }
+}
