97db70eec6871a99f66edfe8b59c89be2e30cafd
Cristian Candela <cristiancandela561@gmail.com>
2026-04-18 15:35:12 -0500

    Mejoras visuales en VistaNosotros y Contactanos

--- frontend/src/app/features/contact/contact.ts
+++ frontend/src/app/features/contact/contact.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common'; // 👈 FALTA ESTO
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule, CommonModule], // 👈 AÑÁDELO AQUÍ
   templateUrl: './contact.html',
-  styleUrl: './contact.css',
+  styleUrls: ['./contact.css']
 })
 export class Contact {
 
+  form = {
+    nombre: '',
+    correo: '',
+    telefono: '',
+    tipo: '',
+    mensaje: ''
+  };
+
+  enviado = false;
+
+  enviar() {
+    console.log(this.form);
+
+    this.enviado = true;
+
+    this.form = {
+      nombre: '',
+      correo: '',
+      telefono: '',
+      tipo: '',
+      mensaje: ''
+    };
+  }
 }
