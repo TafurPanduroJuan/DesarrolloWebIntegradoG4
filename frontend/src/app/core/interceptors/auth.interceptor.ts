6f345b106ac3dd75416c4534420361f032b12d96
JhesuaYola <u23209914@utp.edu.pe>
2026-06-24 22:04:40 -0500

    feat(core): habilitar HttpClient con interceptor JWT

---
+++ frontend/src/app/core/interceptors/auth.interceptor.ts
@@ -0,0 +1,14 @@
+﻿import { HttpInterceptorFn } from '@angular/common/http';
+
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const token = localStorage.getItem('agrolink_token');
+
+  if (token) {
+    const reqConToken = req.clone({
+      setHeaders: { Authorization: 'Bearer ' + token }
+    });
+    return next(reqConToken);
+  }
+
+  return next(req);
+};
