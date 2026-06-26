2b776e41854b45a21dabdafe0cf938abee24a62f
JhesuaYola <u23209914@utp.edu.pe>
2026-06-25 20:13:25 -0500

    chore: eliminar archivos de debug del repo y agregar a gitignore

--- fix_password.sql
+++
@@ -1,1 +0,0 @@
-﻿UPDATE usuario SET password = '$2b$10$FehnLgr5gQyk7Jo3UsQgq.LRoVIFEDYySsR4wBt/RrO9f./lsI2t2';
--- reset.sql
+++
@@ -1,1 +0,0 @@
-﻿UPDATE usuario SET password = '$2a$10$xXsNCgE5kHdZI5o2jpwEuOG6KAs5/ssoeyAiBIuOd5.MepwX.8KPa' WHERE email IN ('agricultor2@agrolink.com','comprador@agrolink.com');
